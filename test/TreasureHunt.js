1f893603842cfde0fa16cfd1e1a192e9ec53a123
Akshit Choudhary <akshitchoudhary007@gmail.com>
2024-09-08 03:00:38 +0530

    Update TreasureHunt.js

--- test/TreasureHunt.js
+++ test/TreasureHunt.js
@@ -8,6 +8,22 @@ describe("TreasureHunt", function () {
   let player1;
   let player2;
 
+  function isValidPrime(num) {
+    // Check if the number is within the valid range
+    if (num < 2 || num > 99) {
+      return false;
+    }
+
+    // Check for primality
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false; // Not a prime number
+      }
+    }
+
+    return true; // It's a prime number
+  }
+
   beforeEach(async function () {
     [owner, player1, player2] = await ethers.getSigners();
     TreasureHunt = await ethers.getContractFactory("TreasureHunt");
@@ -115,14 +131,19 @@ describe("TreasureHunt", function () {
       let playerPosition = Number(
         (await treasureHunt.players(player1.address)).position
       );
+      let moved;
       // Move to the nearest multiple of 5
       while (playerPosition % 5 !== 0) {
         playerPosition = (playerPosition + 1) % 100;
         await treasureHunt.connect(player1).move(playerPosition);
+        moved = true;
       }
-
-      const newTreasurePosition = Number(await treasureHunt.treasurePosition());
-      expect(newTreasurePosition).to.not.equal(initialTreasurePosition);
+      if (moved) {
+        const newTreasurePosition = Number(
+          await treasureHunt.treasurePosition()
+        );
+        expect(newTreasurePosition).to.not.equal(initialTreasurePosition);
+      }
     });
 
     it("Should move treasure to random position when player lands on prime", async function () {
@@ -133,14 +154,20 @@ describe("TreasureHunt", function () {
         (await treasureHunt.players(player1.address)).position
       );
 
-      // Move to the nearest prime number (2, 3, 5, or 7)
-      while (![2, 3, 5, 7].includes(playerPosition)) {
-        playerPosition = (playerPosition + 1) % 100;
-        await treasureHunt.connect(player1).move(playerPosition);
-      }
+      let moved;
 
-      const newTreasurePosition = Number(await treasureHunt.treasurePosition());
-      expect(newTreasurePosition).to.not.equal(initialTreasurePosition);
+      while (!isValidPrime(playerPosition)) {
+        playerPosition = (playerPosition + 1) % 100;
+
+        await treasureHunt.connect(player1).move(playerPosition);
+        moved = true;
+      }
+      if (moved) {
+        const newTreasurePosition = Number(
+          await treasureHunt.treasurePosition()
+        );
+        expect(newTreasurePosition).to.not.equal(initialTreasurePosition);
+      }
     });
   });
 
@@ -155,31 +182,38 @@ describe("TreasureHunt", function () {
     });
 
     it("Should end the game and distribute prize when treasure is found", async function () {
-      const treasurePosition = Number(await treasureHunt.treasurePosition());
+      let treasurePosition = Number(await treasureHunt.treasurePosition());
       let playerPosition = Number(
         (await treasureHunt.players(player1.address)).position
       );
-
+      let finalDiff;
       // Move player1 to the treasure position
       while (playerPosition !== treasurePosition) {
         const diff = (treasurePosition - playerPosition + 100) % 100;
         if (diff === 1 || diff === 99) {
-          playerPosition = treasurePosition;
+          finalDiff = 1;
+          break;
+        } else if (diff === 10) {
+          finalDiff = 10;
+          break;
         } else if (diff < 10) {
           playerPosition = (playerPosition + 1) % 100;
         } else {
           playerPosition = (playerPosition + 10) % 100;
         }
-        await treasureHunt.connect(player1).move(playerPosition);
-      }
 
+        await treasureHunt.connect(player1).move(playerPosition);
+        treasurePosition = Number(await treasureHunt.treasurePosition());
+      }
       const initialBalance = await ethers.provider.getBalance(player1.address);
-      await expect(treasureHunt.connect(player1).move(treasurePosition))
+      await expect(
+        treasureHunt.connect(player1).move(playerPosition + finalDiff)
+      )
         .to.emit(treasureHunt, "GameWon")
         .withArgs(player1.address, ethers.parseEther("0.018")); // 90% of 0.02 ETH
 
       const finalBalance = await ethers.provider.getBalance(player1.address);
-      expect(finalBalance.sub(initialBalance)).to.be.closeTo(
+      expect(finalBalance - initialBalance).to.be.closeTo(
         ethers.parseEther("0.018"),
         ethers.parseEther("0.001") // Allow for gas costs
       );
@@ -214,6 +248,17 @@ describe("TreasureHunt", function () {
         "OwnableUnauthorizedAccount"
       );
     });
+
+    it("Should reset game state after emergency withdraw", async function () {
+      await treasureHunt
+        .connect(player1)
+        .joinGame({ value: ethers.parseEther("0.01") });
+
+      // Emergency withdraw resets the game state
+      await treasureHunt.connect(owner).emergencyWithdraw();
+      expect((await treasureHunt.players(player1.address)).hasJoined).to.be
+        .false;
+    });
   });
 
   describe("Edge cases", function () {
@@ -226,29 +271,6 @@ describe("TreasureHunt", function () {
         .joinGame({ value: ethers.parseEther("0.01") });
     });
 
-    it("Should handle treasure movement when multiple conditions are met", async function () {
-      // Set player position to a prime number that's also a multiple of 5 (e.g., 5)
-      let playerPosition = 5;
-      const initialTreasurePosition = await treasureHunt.treasurePosition();
-
-      // Ensure player1 is at position 4
-      while (
-        Number((await treasureHunt.players(player1.address)).position) != 4
-      ) {
-        await treasureHunt
-          .connect(player1)
-          .move(
-            Number((await treasureHunt.players(player1.address)).position) + 1
-          );
-      }
-
-      // Move to position 5 (prime and multiple of 5)
-      await treasureHunt.connect(player1).move(5);
-
-      const newTreasurePosition = await treasureHunt.treasurePosition();
-      expect(newTreasurePosition).to.not.equal(initialTreasurePosition);
-    });
-
     it("Should handle wrapping around the grid edges", async function () {
       // Move to right edge
       while (
@@ -258,7 +280,9 @@ describe("TreasureHunt", function () {
         await treasureHunt
           .connect(player1)
           .move(
-            Number((await treasureHunt.players(player1.address)).position) + 1
+            (Number((await treasureHunt.players(player1.address)).position) +
+              1) %
+              100
           );
       }
 
@@ -266,7 +290,8 @@ describe("TreasureHunt", function () {
       await treasureHunt
         .connect(player1)
         .move(
-          Number((await treasureHunt.players(player1.address)).position) - 9
+          (Number((await treasureHunt.players(player1.address)).position) + 1) %
+            100
         );
       expect(
         Number((await treasureHunt.players(player1.address)).position) % 10
@@ -295,26 +320,32 @@ describe("TreasureHunt", function () {
     });
 
     it("Should reset game state after a win", async function () {
-      const treasurePosition = Number(await treasureHunt.treasurePosition());
+      let treasurePosition = Number(await treasureHunt.treasurePosition());
       let playerPosition = Number(
         (await treasureHunt.players(player1.address)).position
       );
+      let finalDiff;
 
       // Move player1 to the treasure position
       while (playerPosition !== treasurePosition) {
         const diff = (treasurePosition - playerPosition + 100) % 100;
         if (diff === 1 || diff === 99) {
-          playerPosition = treasurePosition;
+          finalDiff = 1;
+          break;
+        } else if (diff === 10) {
+          finalDiff = 10;
+          break;
         } else if (diff < 10) {
           playerPosition = (playerPosition + 1) % 100;
         } else {
           playerPosition = (playerPosition + 10) % 100;
         }
         await treasureHunt.connect(player1).move(playerPosition);
+        treasurePosition = Number(await treasureHunt.treasurePosition());
       }
 
       // Win the game
-      await treasureHunt.connect(player1).move(treasurePosition);
+      await treasureHunt.connect(player1).move(playerPosition + finalDiff);
 
       // Check game state reset
       expect((await treasureHunt.players(player1.address)).hasJoined).to.be
@@ -325,69 +356,86 @@ describe("TreasureHunt", function () {
         treasurePosition
       );
     });
-
-    it("Should handle multiple players moving in succession", async function () {
-      const initialTreasurePosition = Number(
-        await treasureHunt.treasurePosition()
-      );
-
-      // Both players make 5 moves each
-      for (let i = 0; i < 5; i++) {
-        let player1Position = Number(
-          (await treasureHunt.players(player1.address)).position
-        );
-        await treasureHunt.connect(player1).move((player1Position + 1) % 100);
-
-        let player2Position = Number(
-          (await treasureHunt.players(player2.address)).position
-        );
-        await treasureHunt.connect(player2).move((player2Position + 1) % 100);
-      }
-
-      const finalTreasurePosition = Number(
-        await treasureHunt.treasurePosition()
-      );
-      expect(finalTreasurePosition).to.not.equal(initialTreasurePosition);
-    });
   });
 
   describe("Randomness", function () {
-    it("Should generate different random positions", async function () {
+    it("Should generate different and non-predictable random positions", async function () {
       const positions = new Set();
-
-      for (let i = 0; i < 10; i++) {
-        const wallet = ethers.Wallet.createRandom();
-
-        // Fund the wallet from deployer
+      for (let i = 0; i < 50; i++) {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
         await owner.sendTransaction({
           to: wallet.address,
-          value: ethers.parseEther("0.1"), // Sending 0.1 Ether
+          value: ethers.parseEther("1"),
         });
 
-        // Connect wallet to the provider
-        const connectedWallet = wallet.connect(ethers.provider);
-
         await treasureHunt
-          .connect(connectedWallet)
+          .connect(wallet)
           .joinGame({ value: ethers.parseEther("0.01") });
         const position = Number(
           (await treasureHunt.players(wallet.address)).position
         );
         positions.add(position);
-        // Reset game state
+
+        // Emergency withdraw to reset the state
         await treasureHunt.connect(owner).emergencyWithdraw();
       }
-      expect(positions.size).to.be.gt(1); // Ensure we got at least 2 different positions
+      expect(positions.size).to.be.gt(5); // Expect more than 5 unique positions
+    });
+
+    it("Should ensure randomness can't be manipulated", async function () {
+      const blockNumber = await ethers.provider.getBlockNumber();
+      await treasureHunt
+        .connect(player1)
+        .joinGame({ value: ethers.parseEther("0.01") });
+
+      const initialTreasurePosition = Number(
+        await treasureHunt.treasurePosition()
+      );
+      let playerPosition = Number(
+        (await treasureHunt.players(player1.address)).position
+      );
+      let moved;
+      while (!isValidPrime(playerPosition)) {
+        playerPosition = (playerPosition + 1) % 100;
+        await treasureHunt.connect(player1).move(playerPosition); // Prime number test
+        moved = true;
+      }
+
+      if (moved) {
+        const newBlockNumber = await ethers.provider.getBlockNumber();
+        expect(initialTreasurePosition).to.not.equal(
+          Number(await treasureHunt.treasurePosition())
+        );
+        expect(newBlockNumber).to.be.gt(blockNumber);
+      }
     });
   });
 
   describe("Gas usage", function () {
-    it("Should have reasonable gas costs for joining", async function () {
+    it("Should have reasonable and efficient gas costs for joining", async function () {
       const tx = await treasureHunt
         .connect(player1)
         .joinGame({ value: ethers.parseEther("0.01") });
       const receipt = await tx.wait();
-      expect(receipt.gasUsed).to.be.lt(200000); // Adjust this value based on your requirements
+      expect(receipt.gasUsed).to.be.lt(200000);
+
+      // Test joining efficiency near MAX_PLAYERS
+      for (let i = 0; i < 98; i++) {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+          to: wallet.address,
+          value: ethers.parseEther("1"),
+        });
+        await treasureHunt
+          .connect(wallet)
+          .joinGame({ value: ethers.parseEther("0.01") });
+      }
+
+      const lastJoinTx = await treasureHunt
+        .connect(player2)
+        .joinGame({ value: ethers.parseEther("0.01") });
+      const lastJoinReceipt = await lastJoinTx.wait();
+      expect(lastJoinReceipt.gasUsed).to.be.lt(250000); // Slightly higher limit for last join
     });
 
     it("Should have reasonable gas costs for moving", async function () {
@@ -397,10 +445,37 @@ describe("TreasureHunt", function () {
       const tx = await treasureHunt
         .connect(player1)
         .move(
-          Number((await treasureHunt.players(player1.address)).position) + 1
+          (Number((await treasureHunt.players(player1.address)).position) + 1) %
+            100
         );
       const receipt = await tx.wait();
       expect(receipt.gasUsed).to.be.lt(100000); // Adjust this value based on your requirements
+    });
+
+    it("Should have gas-efficient moves for large number of players", async function () {
+      for (let i = 0; i < 50; i++) {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+          to: wallet.address,
+          value: ethers.parseEther("1"),
+        });
+        await treasureHunt
+          .connect(wallet)
+          .joinGame({ value: ethers.parseEther("0.01") });
+      }
+      // Ensure player1 joins the game
+      await treasureHunt
+        .connect(player1)
+        .joinGame({ value: ethers.parseEther("0.01") });
+      let playerPosition = Number(
+        (await treasureHunt.players(player1.address)).position
+      );
+      playerPosition = (playerPosition + 1) % 100;
+
+      // Ensure move gas usage stays below expected threshold for multiple players
+      const tx = await treasureHunt.connect(player1).move(playerPosition);
+      const receipt = await tx.wait();
+      expect(receipt.gasUsed).to.be.lt(100000);
     });
   });
 });
